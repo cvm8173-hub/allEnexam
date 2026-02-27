c537378cb92fd8dcca68bdefe4d9fc6b0cd3da1c
cvm8173-hub <cvm8173@gmail.com>
2026-02-27 15:12:08 +0530

    Update allexam.py

--- allexam.py
+++ allexam.py
@@ -2195,7 +2195,7 @@ if __name__ == "__main__":
     try:
         final_data = []
 
-        for page in range(1, 4): 
+        for page in range(4, 8): 
             print(f"Scraping listing page {page}")
             
             exams = scrape_listing_page(driver, page)  # Make scrape_listing_page accept page param
